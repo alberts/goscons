a9d7760d6817041c40b7d68419160116393c74f3
alberts <alberts@nextbuild.vastech.co.za>
2010-09-17 17:40:41 +0200

    gccgo closer to working. Not scanning yet, so targets could build in wrong order.

--- goscons/gccgo.py
+++ goscons/gccgo.py
@@ -2,10 +2,14 @@ import SCons.Util
 
 def generate(env):
     env['GOC'] = 'gccgo'
+    env['GOLINK'] = 'gccgo'
     env['GOOBJSUFFIX'] = '.o'
-    env['GOCCOM'] = '$GOC $GOCFLAGS -c -o $TARGET $SOURCES'
-    env['GOPACK'] = 'ar'
-    env['GOPACKFLAGS'] = SCons.Util.CLVar('cru')
+    env['GOLIBSUFFIX'] = '.gox'
+    env['GOCCOM'] = '$GOC -pipe $GOCFLAGS ${_concat("-I ", GOPKGPATH, "", __env__)} -c -o $TARGET $SOURCES'
+    env['GOLINKCOM'] = '$GOC -pipe $GOLINKFLAGS ${_concat("-I ", GOPKGPATH, "", __env__)} ${_concat("-Wl,-rpath ", GORPATH, "", __env__)} -o $TARGET $SOURCES'
+    env['GOPACK'] = 'objcopy'
+    env['GOPACKFLAGS'] = SCons.Util.CLVar('')
+    env['GOPACKCOM'] = '$GOPACK -j .go_export $GOPACKFLAGS $SOURCES $TARGET'
 
 def exists(env):
     return 1
