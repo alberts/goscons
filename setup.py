aca0fc45d8fb8e78fb0c8934091f3e6fe63b0620
Albert Strasheim <fullung@gmail.com>
2010-11-04 15:25:17 +0200

    Added Python setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='goscons',
+      version='1.0',
+      author='Albert Strasheim',
+      author_email='fullung@gmail.com',
+      url='http://github.com/alberts/goscons/',
+      packages=['goscons'])
