d21d5bbdd3a8f957db350bf8fd8046e90004cdcc
Albert Strasheim <fullung@gmail.com>
2010-12-03 16:27:26 +0200

    gccgo fixes.

--- goscons/gccgo.py
+++ goscons/gccgo.py
@@ -1,4 +1,5 @@
 import SCons.Util
+import goutils
 import os
 
 IMPORTS = (('', '.gox'), ('', '.o'), ('lib', '.so'), ('lib', '.a'))
@@ -12,22 +13,30 @@ def find_package(env, pkg, path):
             return pkgpath
     return None
 
-def _go_prefix(source):
-    # TODO figure out why this attribute sometimes has more than one
-    # element in its list
-    return source.attributes.go_package[0]
+def _go_prefix(source, env):
+    prefix = env.subst('${GOPREFIX}') + goutils.package_name(source[0], env)
+    if len(prefix) > 0:
+        return '-fgo-prefix=' + prefix
+    return ''
+
+def _go_rpath(lst, env, f=lambda x: x, target=None, source=None):
+    if not lst: return lst
+    l = f(SCons.PathList.PathList(lst).subst_path(env, target, source))
+    if l is not None: lst = l
+    return ' '.join(['-Wl,-rpath,' + p.abspath for p in lst])
 
 def generate(env):
     env['_go_prefix'] = _go_prefix
+    env['_go_rpath'] = _go_rpath
     env['GCCGOPREFIX'] = '/usr/local'
     env['GOC'] = 'gccgo'
     env['GOLINK'] = 'gccgo'
     env['GOOBJSUFFIX'] = '.o'
     env['GOLIBPREFIX'] = 'lib'
     env['GOLIBSUFFIX'] = '.a'
-    env['GOCCOM'] = '$GOC -fgo-prefix=${GOPREFIX}${_go_prefix(SOURCES)} -pipe $GOCFLAGS ${_concat("-I ", GOPKGPATH, "", __env__)} -c -o $TARGET $SOURCES'
-    # TODO use abspath for rpath
-    env['GOLINKCOM'] = '$GOC -pipe -static -pthread $GOLINKFLAGS ${_concat("-I ", GOPKGPATH, "", __env__)} ${_concat("-Wl,-rpath,", GORPATH, "", __env__)} -o $TARGET $SOURCES -lgobegin -lgo'
+    env['GOCCOM'] = '$GOC ${_go_prefix(SOURCES, __env__)} -pipe $GOCFLAGS ${_concat("-I ", GOPKGPATH, "", __env__)} -c -o $TARGET $SOURCES'
+    #env['GOLINKCOM'] = '$GOC -pipe -static -pthread $GOLINKFLAGS ${_concat("-I ", GOPKGPATH, "", __env__)} ${_go_rpath(GORPATH, __env__)} -o $TARGET $SOURCES -lgobegin -lgo'
+    env['GOLINKCOM'] = '$GOC -pipe -static -pthread $GOLINKFLAGS ${_go_rpath(GORPATH, __env__)} -o $TARGET $SOURCES -lgobegin -lgo'
     env['GOPACK'] = 'ar'
     env['GOPACKFLAGS'] = SCons.Util.CLVar('cru')
     env['GOPACKCOM'] = '$GOPACK $GOPACKFLAGS $TARGET $SOURCES'
